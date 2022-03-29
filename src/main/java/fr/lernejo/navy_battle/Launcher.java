aba8cf2228ed6f1166aaf9e7cb85943a68880b44
William Barbosa <william.barbosa-souto-amado@epita.fr>
2022-03-29 23:14:52 +0200

    ping handler adress already in use..

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,54 +1,26 @@
 package fr.lernejo.navy_battle;
-import fr.lernejo.PingHandler;
-
 import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 
 public class Launcher
 {
-    public static void main(String[] args) throws IOException
-    {
-        final Map<String, String> gameContext = new HashMap<String, String>();
-        if (args.length < 1)
-            return;
-        int port = parse_port(args);
-        UUID uuid = UUID.randomUUID();
-        gameContext.put("my_id", uuid.toString());
-        gameContext.put("my_port", String.valueOf(port));
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 1);
-        ExecutorService executorService =
-            new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>());
-        beginServer(port);
-    }
-
-    private static void beginServer(int port) throws IOException
+    private static void startServer(int port) throws IOException
     {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-        ExecutorService executorService = new ThreadPoolExecutor(1, 1, 1000, TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<Runnable>());
+        httpServer.setExecutor(Executors.newSingleThreadExecutor());
         httpServer.createContext("/ping", new PingHandler());
-        httpServer.setExecutor(executorService);
         httpServer.start();
     }
-    private static int parse_port(String[] args)
-    {
-        try
-        {
-            return Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-        return 0;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0)
+            return;
+        int serverPort = Integer.parseInt(args[0]);
+        System.out.println("Listening on port :" + serverPort);
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        startServer(serverPort);
     }
 }
--- src/main/java/fr/lernejo/navy_battle/PingHandler.java
+++ src/main/java/fr/lernejo/navy_battle/PingHandler.java
@@ -1,20 +1,20 @@
-package fr.lernejo;
-
+package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class PingHandler implements HttpHandler {
-        String body = "Hello";
-    public void handle(HttpExchange exchange) throws IOException {
-        exchange.sendResponseHeaders(200,body.length());
-        try(
-            OutputStream os = exchange.getResponseBody())
 
-            {
-                os.write(body.getBytes());
-            }
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String body = "OK";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(body.getBytes());
+        }
+        exchange.close();
     }
 }
