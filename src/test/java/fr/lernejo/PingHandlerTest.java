cda0fa502fcaa44dfe9640c679ed8d00f76ce211
William Barbosa <william.barbosa-souto-amado@epita.fr>
2022-03-30 00:24:44 +0200

    checking coverage

---
+++ src/test/java/fr/lernejo/PingHandlerTest.java
@@ -0,0 +1,32 @@
+package fr.lernejo.navy_battle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import java.io.IOException;
+
+class LauncherTest {
+
+    @org.junit.jupiter.api.Test
+    void PingTests() throws IOException, InterruptedException {
+        String[] args = {"9876"};
+        Launcher.main(args);
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        URL url = new URL("http://localhost:9876/ping");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        Assertions.assertEquals(status, 200);
+    }
+}
